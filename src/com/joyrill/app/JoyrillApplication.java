1adeb21722b552b77ba46e968cef26ab1b043ffb
heyi <308068086@qq.com>
2013-09-14 16:34:58 +0800

    update

--- src/com/joyrill/app/JoyrillApplication.java
+++ src/com/joyrill/app/JoyrillApplication.java
@@ -12,39 +12,39 @@ import com.joyrill.app.exception.JoyrillException;
 import dalvik.system.VMRuntime;
 
 public class JoyrillApplication extends Application {
-	
+
 	public static final int NETTYPE_WIFI = 0x01;
 	public static final int NETTYPE_CMWAP = 0x02;
 	public static final int NETTYPE_CMNET = 0x03;
-	
+
 	private static Context context;
 
 	JoyrillApplicationInitialize initialize;
 
 	static {
-		
+
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
-		Thread.setDefaultUncaughtExceptionHandler(JoyrillException.getAppExceptionHandler());
-		
+
+		Thread.setDefaultUncaughtExceptionHandler(JoyrillException
+				.getAppExceptionHandler());
+
 		final int HEAP_SIZE = 64 * 1024 * 1024;
 		VMRuntime.getRuntime().setMinimumHeapSize(HEAP_SIZE);
-		
+
 		context = this;
 		this.setEnviroment();
 		initialize.initDatabase();
 		initialize.initLanguage();
 		initialize.initCache();
 		initialize.initData();
-		
-        JPushInterface.setDebugMode(true);
+
+		JPushInterface.setDebugMode(true);
 		JPushInterface.init(this);
 	}
-
 
 	public static Context getInstance() {
 		return context;
@@ -77,7 +77,8 @@ public class JoyrillApplication extends Application {
 	}
 
 	public String getAppId() {
-		TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);  
+		TelephonyManager tm = (TelephonyManager) this
+				.getSystemService(TELEPHONY_SERVICE);
 		return tm.getDeviceId();
 	}
 }
--- src/com/joyrill/app/JoyrillApplicationInitialize.java
+++ src/com/joyrill/app/JoyrillApplicationInitialize.java
@@ -23,63 +23,66 @@ public class JoyrillApplicationInitialize {
 	public static String dbname = "zjdata";
 	private Context context;
 	private Cache cache = Cache.getInstance();
-	private File dbfile = null,dir = null;
+	private File dbfile = null, dir = null;
 	private SharedPreferencesHelper helper = null;
-	public JoyrillApplicationInitialize(Context context){
+
+	public JoyrillApplicationInitialize(Context context) {
 		this.context = context;
 		this.helper = new SharedPreferencesHelper(context);
 	}
-	
+
 	public synchronized void initDatabase() {
-		
-    	dbfile = context.getDatabasePath("zjdata.db");
-    	dir = dbfile.getParentFile();  
-        while(!dir.exists()){  
-        	dir.mkdirs();
-        }  
-        if(!dbfile.exists()){
-        	InputStream input = null;
-        	OutputStream output = null;
-        	
-        	try {
-        		Logger.d("start copy database from assert!");
-        		input = context.getResources().getAssets().open(dbname);
-        		output = new FileOutputStream(dbfile);
-        		byte[] buffer = new byte[2048];
-        		int length;
-        		while ((length = input.read(buffer)) > 0) {
-        			output.write(buffer, 0, length);
-        		}
-        		Logger.d("copy database success !");
-        	} catch (Exception e) {
-        		Logger.e(e.getMessage());
-        	}
-        }
-        
+
+		dbfile = context.getDatabasePath("zjdata.db");
+		dir = dbfile.getParentFile();
+		while (!dir.exists()) {
+			dir.mkdirs();
+		}
+		if (!dbfile.exists()) {
+			InputStream input = null;
+			OutputStream output = null;
+
+			try {
+				Logger.d("start copy database from assert!");
+				input = context.getResources().getAssets().open(dbname);
+				output = new FileOutputStream(dbfile);
+				byte[] buffer = new byte[2048];
+				int length;
+				while ((length = input.read(buffer)) > 0) {
+					output.write(buffer, 0, length);
+				}
+				Logger.d("copy database success !");
+			} catch (Exception e) {
+				Logger.e(e.getMessage());
+			}
+		}
+
 	}
-    public void initLanguage() {
-    	Configuration config = context.getResources().getConfiguration(); 
-    	DisplayMetrics dm = context.getResources() .getDisplayMetrics(); 
-    	String able= context.getResources().getConfiguration().locale.getCountry(); 
-    	String language = helper.getValue("language", able);
-    	if("en".equalsIgnoreCase(language)){
-    		config.locale = Locale.ENGLISH; 
-    	}else if("CN".equalsIgnoreCase(language)){
-    		config.locale = Locale.SIMPLIFIED_CHINESE; 
-    	}else{
-    		
-    	}
-    	context.getResources().updateConfiguration(config, dm); 
-    }
+
+	public void initLanguage() {
+		Configuration config = context.getResources().getConfiguration();
+		DisplayMetrics dm = context.getResources().getDisplayMetrics();
+		String able = context.getResources().getConfiguration().locale
+				.getCountry();
+		String language = helper.getValue("language", able);
+		if ("en".equalsIgnoreCase(language)) {
+			config.locale = Locale.ENGLISH;
+		} else if ("CN".equalsIgnoreCase(language)) {
+			config.locale = Locale.SIMPLIFIED_CHINESE;
+		} else {
+
+		}
+		context.getResources().updateConfiguration(config, dm);
+	}
 
 	public void initCache() {
-		//cache.putValue(Cache.USER_CACHE, User.findDefaultUser());
-		cache.putValue(Cache.USER_CACHE, new User("admin","123456"));
+		// cache.putValue(Cache.USER_CACHE, User.findDefaultUser());
+		cache.putValue(Cache.USER_CACHE, new User("admin", "123456"));
 
 	}
-	
-	public void initData(){
-		if(null == helper.getValue("FirstUse")){
+
+	public void initData() {
+		if (null == helper.getValue("FirstUse")) {
 			helper.putValue("FirstUse", "0");
 			ParameterOperation.autoLogin(false);
 		}
